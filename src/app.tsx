f1c1a1dcd78bca7c59e9139db1e0ba7a598f5361
justheimsk <henrique.mdimer@gmail.com>
2024-09-03 19:08:06 -0300

    chore: render components

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,59 @@
+import { Navbar } from './components/layout/Navbar';
+import { UpperNavbar } from './components/layout/UpperNavbar';
 import './global.css';
 
 const App = () => {
   return (
-    <div className="content">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
-    </div>
+    <>
+      <UpperNavbar content="Disponível para contratação" />
+      <Navbar />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+    </>
   );
 };
 
