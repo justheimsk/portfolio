2abf2d5f56bca96598515493df61708165bf1775
justheimsk <henrique.mdimer@gmail.com>
2024-09-06 14:27:07 -0300

    refactor: change button hover color

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,3 +1,3 @@
 export function Button(props: React.HTMLAttributes<HTMLButtonElement>) {
-  return <button {...props} className={`bg-white rounded-full text-black px-2 py-1 text-sm font-bold hover:bg-white/80 transition ${props.className}`}>{props.children}</button>
+  return <button {...props} className={`bg-white rounded-full text-black px-2 py-1 text-sm font-bold hover:bg-gray-300 transition ${props.className}`}>{props.children}</button>
 }
