3c951267d4c0fddca1524405e6c99f4680929e45
justheimsk <henrique.mdimer@gmail.com>
2024-09-08 15:59:32 -0300

    build: enable chunk splitting

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -3,6 +3,11 @@ import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
   plugins: [pluginReact()],
+  performance: {
+    chunkSplit: {
+      strategy: 'split-by-experience'
+    }
+  },
   html: {
     template: './index.html'
   }
