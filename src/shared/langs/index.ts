079f27eb18f74e2ef4070c1584dc3bbf282462de
justheimsk <henrique.mdimer@gmail.com>
2024-09-10 23:20:58 -0300

    feat: add several translations

--- src/shared/langs/index.ts
+++ src/shared/langs/index.ts
@@ -1,7 +1,21 @@
 import EN from './en.json';
 import PTBR from './pt-br.json';
+import DEDE from './de-de.json';
+import FRFR from './fr-fr.json';
+import ITIT from './it-it.json';
+import RURU from './ru-ru.json';
+import NLNL from './nl-nl.json'
+import GAIE from './ga-ie.json';
+import SVSE from './sv-se.json';
 
 export default {
   "en-US": EN,
+  "de-DE": DEDE,
+  "fr-FR": FRFR,
+  "it-IT": ITIT,
   "pt-BR": PTBR,
+  "ru-RU": RURU,
+  "nl-NL": NLNL,
+  "ga-IE": GAIE,
+  "sv-SE": SVSE
 }
