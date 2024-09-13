e959105bfb532759720eef52991255d047969b86
justheimsk <henrique.mdimer@gmail.com>
2024-09-13 14:09:52 -0300

    feat: add support for links and redirect

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -6,8 +6,20 @@ export interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
   bstyle?: ButtonStyles;
   rounded?: ButtonRounded;
   bsize?: ButtonSize;
+  bhref?: string;
 }
 
 export function Button(props: ButtonProps) {
-  return <button {...props} className={`${(props.bstyle || "normal") === "normal" ? "bg-white text-black" : " text-white bg-transparent border border-white hover:text-black"} flex items-center justify-center gap-2 ${(props.bsize || "normal") === 'normal' ? 'px-2 py-1 text-sm' : 'px-3 py-2 text-md'} font-bold hover:bg-gray-300 transition ${props.rounded || "rounded-md"} ${props.className}`}>{props.children}</button>
+  const classname = `${(props.bstyle || "normal") === "normal" ? "bg-white text-black" : " text-white bg-transparent border border-white hover:text-black"} flex items-center justify-center gap-2 ${(props.bsize || "normal") === 'normal' ? 'px-2 py-1 text-sm' : 'px-3 py-2 text-md'} font-bold hover:bg-gray-300 transition ${props.rounded || "rounded-md"} ${props.className}`;
+
+  return (
+    <>
+      {!props.bhref  ? (
+        <button {...props} className={classname}>{props.children}</button>
+      ) : (
+        <a target="_blank" rel="noreferrer" href={props.bhref} className={classname}>{props.children}</a>
+        )
+      }
+    </>
+  )
 }
