2dbd9574e1e671679a6bf87ef4ea4ff775e45ba0
justheimsk <henrique.mdimer@gmail.com>
2024-09-14 18:07:07 -0300

    feat: add Tape component

---
+++ src/components/common/Tape.tsx
@@ -0,0 +1,45 @@
+import { FaStar } from 'react-icons/fa6';
+
+const words = [
+  'PERFORMANT',
+  'SECURE',
+  'STABLE',
+  'FAST',
+  'RESPONSIVE',
+  'OPTIMIZED',
+  'USABLE',
+  'RELIABLE',
+  'SCALABLE',
+  'MAINTAINABLE',
+  'ACCESSIBLE',
+];
+
+export function WordsComponent() {
+  return (
+    <>
+      {words.map((word) => (
+        <div key={Math.random() * 99999} className="flex items-center gap-6">
+          <FaStar />
+          <span>{word}</span>
+        </div>
+      ))}
+    </>
+  );
+}
+
+export function Tape() {
+  return (
+    <div className="w-svw max-w-[100vw] mr-6 text-white font-bold h-12 gradient-secondary -rotate-3">
+      <div className="flex w-svw h-full [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+        <div className="infinite-loop w-svw flex items-center gap-6 h-full">
+          <WordsComponent />
+          <WordsComponent />
+          <WordsComponent />
+          <WordsComponent />
+          <WordsComponent />
+          <WordsComponent />
+        </div>
+      </div>
+    </div>
+  );
+}
