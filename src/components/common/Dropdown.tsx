ff56326234d4a2b8a8f2ac9e097b1694e7518a1b
justheimsk <henrique.mdimer@gmail.com>
2024-09-15 13:43:09 -0300

    fix: dropdown hover not working

--- src/components/common/Dropdown.tsx
+++ src/components/common/Dropdown.tsx
@@ -8,7 +8,7 @@ export function Menu(props: React.HTMLAttributes<HTMLDivElement>) {
 
 export function Content(props: React.HTMLAttributes<HTMLDivElement>) {
   return (
-    <div className="absolute top-[65%] translate-y-[5px] left-1/2 translate-x-[-50%] rounded-md border border-white/10 bg-white/10 backdrop-blur w-44 min-h-6 opacity-0 transition duration-300 group-hover:opacity-100 pointer-events-none group-hover:pointer-events-auto group-hover:translate-y-0 shadow-lg p-1 flex flex-col gap-1">
+    <div className="absolute top-[65%] translate-y-[5px] left-1/2 translate-x-[-50%] rounded-md border border-white/10 bg-white/10 backdrop-blur w-44 min-h-6 opacity-0 transition duration-300 group-hover:opacity-100 pointer-events-none group-hover:pointer-events-auto group-hover:translate-y-0 shadow-lg p-1 flex flex-col gap-1 z-50">
       {props.children}
     </div>
   )
