e9f8ecc9b8cacffb28aa33ea964841a0f4882ea2
justheimsk <henrique.mdimer@gmail.com>
2024-09-17 14:37:28 -0300

    chore: render env variable name

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -7,7 +7,7 @@ import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { IoLanguage } from 'react-icons/io5';
 import * as Dropdown from '../common/Dropdown';
-import langs from "../../shared/langs";
+import langs from '../../shared/langs';
 
 export function Navbar() {
   const [active, setActive] = useState(false);
@@ -21,29 +21,46 @@ export function Navbar() {
             <NavbarMenu />
           </div>
           <div className="flex md:hidden items-center h-full text-white justify-between px-4">
-            <span className="font-semibold">Vonderheimsk</span>
+            <NavbarBrandName />
             <div className="flex items-center gap-2">
-              <Button clickEffect="shrink" rounded="rounded-full" className="font-semibold text-[12px] px-2 py-0.5">
+              <Button
+                clickEffect="shrink"
+                rounded="rounded-full"
+                className="font-semibold text-[12px] px-2 py-0.5"
+              >
                 {t('buttons.resume')}
               </Button>
-              <RxHamburgerMenu onClick={() => setActive(!active)} className="cursor-pointer text-xl" />
+              <RxHamburgerMenu
+                onClick={() => setActive(!active)}
+                className="cursor-pointer text-xl"
+              />
             </div>
           </div>
         </nav>
       </div>
-      <div className={`fixed transition top-0 z-[51] md:hidden left-0 w-full h-full bg-white/20 backdrop-blur text-lg font-bold flex flex-col items-center gap-4 pt-4 ${active ? "pointer-events-auto opacity-100 translate-y-0" : "pointer-events-none opacity-0 translate-y-4"}`}>
+      <div
+        className={`fixed transition top-0 z-[51] md:hidden left-0 w-full h-full bg-white/20 backdrop-blur text-lg font-bold flex flex-col items-center gap-4 pt-4 ${active ? 'pointer-events-auto opacity-100 translate-y-0' : 'pointer-events-none opacity-0 translate-y-4'}`}
+      >
         <NavbarMenu />
-        <FaTimes onClick={() => setActive(false)} className="absolute top-4 right-4 text-2xl text-white cursor-pointer" />
+        <FaTimes
+          onClick={() => setActive(false)}
+          className="absolute top-4 right-4 text-2xl text-white cursor-pointer"
+        />
       </div>
       <Container className="hidden md:block absolute top-10 w-full left-1/2 translate-x-[-50%]">
         <div className="w-full flex items-center justify-between h-10 text-white">
-          <span className="font-semibold text-lg">Vonderheimsk</span>
+          <NavbarBrandName />
           <div className="flex gap-4 items-center">
             <Dropdown.Menu>
               <IoLanguage className="nav-icon" />
               <Dropdown.Content>
                 {Object.values(langs).map((lang) => (
-                  <Dropdown.Item onClick={() => i18n.changeLanguage(lang.meta.code)} key={lang.meta.code}>{lang.meta.name}</Dropdown.Item>
+                  <Dropdown.Item
+                    onClick={() => i18n.changeLanguage(lang.meta.code)}
+                    key={lang.meta.code}
+                  >
+                    {lang.meta.name}
+                  </Dropdown.Item>
                 ))}
               </Dropdown.Content>
             </Dropdown.Menu>
@@ -56,10 +73,35 @@ export function Navbar() {
               <FaGithub className="nav-icon" />
             </a>
             <FaDiscord className="nav-icon text-[#5865F2]" />
-            <Button clickEffect="shrink" rounded="rounded-full">{t('buttons.resume')}</Button>
+            <Button clickEffect="shrink" rounded="rounded-full">
+              {t('buttons.resume')}
+            </Button>
           </div>
         </div>
       </Container>
+    </>
+  );
+}
+
+export function NavbarBrandName() {
+  return (
+    <span className="font-semibold text-sm md:text-lg capitalize">
+      {process.env.PUBLIC_NAME}
+    </span>
+  )
+}
+
+export function NavbarMenu() {
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <NavbarLink nhref="home" active>
+        {t('navbar.home')}
+      </NavbarLink>
+      <NavbarLink nhref="projects">{t('navbar.projects')}</NavbarLink>
+      <NavbarLink>{t('navbar.skills')}</NavbarLink>
+      <NavbarLink>{t('navbar.contact')}</NavbarLink>
     </>
   );
 }
@@ -70,24 +112,11 @@ export interface NavbarLinkProps
   nhref?: string;
 }
 
-export function NavbarMenu() {
-  const { t } = useTranslation();
-
-  return (
-    <>
-      <NavbarLink nhref="home" active>{t('navbar.home')}</NavbarLink>
-      <NavbarLink nhref="projects">{t('navbar.projects')}</NavbarLink>
-      <NavbarLink>{t('navbar.skills')}</NavbarLink>
-      <NavbarLink>{t('navbar.contact')}</NavbarLink>
-    </>
-  );
-}
-
 export function NavbarLink(props: NavbarLinkProps) {
   function scrollTo() {
-    if(props.nhref) {
+    if (props.nhref) {
       const element = document.getElementById(props.nhref);
-      element?.scrollIntoView({ behavior: "smooth" });
+      element?.scrollIntoView({ behavior: 'smooth' });
     }
   }
 
