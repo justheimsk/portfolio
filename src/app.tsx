f77f6a767a538a7d14bbbacc87dea86cf11921f6
justheimsk <henrique.mdimer@gmail.com>
2024-09-17 13:44:52 -0300

    fix: wrong speed insights import

--- src/app.tsx
+++ src/app.tsx
@@ -22,7 +22,7 @@ import { useTranslation } from 'react-i18next';
 import { ProjectCard } from './components/common/ProjectCard';
 import { Tape } from './components/common/Tape';
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 const App = () => {
   const { t } = useTranslation();
