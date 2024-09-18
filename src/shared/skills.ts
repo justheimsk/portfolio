c5d08a7cb76cf6e4869c70492ba31d1486c8b4b4
justheimsk <henrique.mdimer@gmail.com>
2024-09-17 21:21:14 -0300

    chore: add skills

---
+++ src/shared/skills.ts
@@ -0,0 +1,85 @@
+export const Frontend = [
+  {
+    img: 'react.png',
+    name: "React"
+  },
+  {
+    img: 'nextjs.webp',
+    name: 'NextJS'
+  },
+  {
+    img: "html.png",
+    name: "HTML"
+  },
+  {
+    img: "css.png",
+    name: "CSS"
+  },
+  {
+    img: "SASS.png",
+    name: "SASS"
+  },
+  {
+    img: "redux.png",
+    name: "Redux"
+  },
+  {
+    img: "tailwind.png",
+    name: "TailwindCSS"
+  },
+  {
+    img: "git.png",
+    name: "Git"
+  },
+  {
+    img: "vue.png",
+    name: "Vue"
+  },
+  {
+    img: "gsap.png",
+    name: "GSAP"
+  },
+]
+
+export const Backend = [
+  {
+    img: 'typescript.png',
+    name: 'Typescript'
+  },
+  {
+    img: 'Clang.png',
+    name: 'C'
+  },
+  {
+    img: 'Javascript.png',
+    name: "Javascript"
+  },
+  {
+    img: "docker.png",
+    name: "Docker"
+  },
+  {
+    img: "nodejs.png",
+    name: "Nodejs"
+  },
+  {
+    img: "postgres.png",
+    name: "PostgreSQL"
+  },
+  {
+    img: "linux.png",
+    name: "Linux"
+  },
+  {
+    img: "mongodb.png",
+    name: "MongoDB"
+  },
+  {
+    img: "libuv.png",
+    name: "Libuv"
+  },
+  {
+    img: "nginx.png",
+    name: "Nginx"
+  },
+]
